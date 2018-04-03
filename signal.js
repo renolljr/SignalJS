78c80d9c8ff7983c54f5ecd59fb423320a319064
L A Renoll Jr <8606519+renolljr@users.noreply.github.com>
2018-04-02 20:45:42 -0400

    Setting default parameters and passing them in.   For now better than creating class.

--- signal.js
+++ signal.js
@@ -1,165 +1,152 @@
 
 
 // JavaScript source code
-const s = [
-	[5, 0, 25, 5, 145, 250],
-	[0, 5, 95, 115, 165, 250],
-	[15, 5, 175, 250, 185, 160],
-	[5, 0, 145, 250, 245, 140],
-	[115, 210, 60, 5, 230, 220],
-	[0, 80, 45, 95, 170, 145]
-]
 
-const threshold = 200;
+function main(s = [
+	                [5, 0, 25, 5, 145, 250],
+	                [0, 5, 95, 115, 165, 250],
+	                [15, 5, 175, 250, 185, 160],
+	                [5, 0, 145, 250, 245, 140],
+	                [115, 210, 60, 5, 230, 220],
+	                [0, 80, 45, 95, 170, 145]
+                ],threshold = 200){
 
-
-const doWork = (s, threshold) =>
-{
-
-}
-
-const hasTop = (point) => {
-    let [i,j] = point;
-    if(i > s.length - 1) return false;
-    console.log(s[i+1][j]);
-    if(s[i+1][j] >= threshold){
-        return true;
+    const hasTop = (point) => {
+        let [i,j] = point;
+        if(i > s.length - 1) return false;
+        if(s[i+1][j] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const hasBottom = (point) => {
-    let [i,j] = point;
-    if(i<1) return false;
-    if(s[i-1][j] >= threshold){
-        return true;
+    const hasBottom = (point) => {
+        let [i,j] = point;
+        if(i<1) return false;
+        if(s[i-1][j] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const hasLeft = (point) => {
-    let [i,j] = point;
-    if(j<1) return false;
-    if(s[i][j-1] >= threshold){
-        return true;
+    const hasLeft = (point) => {
+        let [i,j] = point;
+        if(j<1) return false;
+        if(s[i][j-1] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const hasRight = (point) => {
-    let [i,j] = point;
-    if(j > s.length - 1) return false;
-    if(s[i][j+1] >= threshold){
-        return true;
+    const hasRight = (point) => {
+        let [i,j] = point;
+        if(j > s.length - 1) return false;
+        if(s[i][j+1] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-
-const hasTopLeft = (point) => {
-    let [i,j] = point;
-    if(i > s.length - 1) return false;
-    if(j < 1) return false;
-    if(s[i+1][j-1] >= threshold){
-        return true;
+    const hasTopLeft = (point) => {
+        let [i,j] = point;
+        if(i > s.length - 1) return false;
+        if(j < 1) return false;
+        if(s[i+1][j-1] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const hasBottomLeft = (point) => {
-    let [i,j] = point;
-    if(i < 1) return false;
-    if(j < 1) return false;
-    if(s[i-1][j-1] >= threshold){
-        return true;
+    const hasBottomLeft = (point) => {
+        let [i,j] = point;
+        if(i < 1) return false;
+        if(j < 1) return false;
+        if(s[i-1][j-1] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const hasTopRight = (point) => {
-    let [i,j] = point;
-    if(i > s.length - 1) return false;
-    if(j > s.length - 1) return false;
-    if(s[i+1][j+1] >= threshold){
-        return true;
+    const hasTopRight = (point) => {
+        let [i,j] = point;
+        if(i > s.length - 1) return false;
+        if(j > s.length - 1) return false;
+        if(s[i+1][j+1] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const hasBottomRight = (point) =>{
-    let [i,j] = point;
-    if(i < 1) return false;
-    if(j > s.length - 1) return false;
-    if(s[i-1][j+1] >= threshold){
-        return true;
+    const hasBottomRight = (point) =>{
+        let [i,j] = point;
+        if(i < 1) return false;
+        if(j > s.length - 1) return false;
+        if(s[i-1][j+1] >= threshold){
+            return true;
+        }
+        else
+            return false;
     }
-    else
-        return false;
-}
 
-const getTop = (point) => {
-    let [i,j] = point; 
-    return s[i+1][j]; 
-}
+    const getTop = (point) => {
+        let [i,j] = point; 
+        return s[i+1][j]; 
+    }
 
-const getBottom = (point) => {
-    let [i,j] = point;
-    return s[i-1][j]
-}
+    const getBottom = (point) => {
+        let [i,j] = point;
+        return s[i-1][j]
+    }
 
-const getLeft = (point) => {
-    let [i,j] = point;
-    return s[i][j-1];
-}
+    const getLeft = (point) => {
+        let [i,j] = point;
+        return s[i][j-1];
+    }
 
-const getRight = (point) => {
-    let [i,j] = point;
-    return s[i][j+1];
-}
+    const getRight = (point) => {
+        let [i,j] = point;
+        return s[i][j+1];
+    }
 
-const getTopLeft = (point) => {
-    let [i,j] = point;
-    return s[i+1][j-1];
-}
+    const getTopLeft = (point) => {
+        let [i,j] = point;
+        return s[i+1][j-1];
+    }
 
-const getBottomLeft = (point) => {
-    let [i,j] = point;
-    return s[i-1][j-1];
-}
+    const getBottomLeft = (point) => {
+        let [i,j] = point;
+        return s[i-1][j-1];
+    }
 
-const getTopRight = (point) => {
-    let [i,j] = point;
-    return s[i+1][j+1];
-}
+    const getTopRight = (point) => {
+        let [i,j] = point;
+        return s[i+1][j+1];
+    }
    
-const getBottomRight = (point) => {
-    let [i,j] = point;
-    return s[i-1][j+1];
-}
+    const getBottomRight = (point) => {
+        let [i,j] = point;
+        return s[i-1][j+1];
+    }
     
-const getMarkovBlanket = (point) => {
-   return [   
-        hasTop(point) ? getTop(point) : undefined,
-        hasBottom(point) ? getBottom(point) : undefined,
-        hasLeft(point) ? getLeft(point) : undefined,
-        hasRight(point) ? getRight(point) : undefined,
-        hasTopLeft(point) ? getTopLeft(point) : undefined,
-        hasBottomLeft(point) ? getBottomLeft(point) : undefined,
-        hasTopRight(point) ? getBottomRight(point) : undefined,
-        hasBottomRight(point) ? getBottomRight(point) : undefined 
-    ]
-};
-
-function main(s,threshold){
-    //t is threshold
-    //s is 2-D array, array of arrays
+    const getMarkovBlanket = (point) => {
+        return [   
+             hasTop(point) ? getTop(point) : undefined,
+             hasBottom(point) ? getBottom(point) : undefined,
+             hasLeft(point) ? getLeft(point) : undefined,
+             hasRight(point) ? getRight(point) : undefined,
+             hasTopLeft(point) ? getTopLeft(point) : undefined,
+             hasBottomLeft(point) ? getBottomLeft(point) : undefined,
+             hasTopRight(point) ? getBottomRight(point) : undefined,
+             hasBottomRight(point) ? getBottomRight(point) : undefined 
+        ]
+    };
 
     const size = s.length; // assume s.length and s[0].length size are fixed.
     
@@ -178,7 +165,13 @@ function main(s,threshold){
        
         let mb = getMarkovBlanket(point);
         console.log(mb);
+        for(let section of mb){
+            if(typeof(section) !== "undefined"){
+                console.log(section);
+            }
+
+        }
     }
 }
 
-main(s,200);
+main();
